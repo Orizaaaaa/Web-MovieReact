ed38ed81b4e68bf5f88676ef23ece8794a157267
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-08-24 11:57:49 +0700

    add progress bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AuthLayout from '../components/Layout/AuthLayout'
 import { caraosel } from '../image'
 
 function Home() {
+
     return (
         <AuthLayout>
             <section className='homePage' >
                 <div className="moviesCaraosel d-flex  ">
                     <div className="title">
-                        <p>babi</p>
+                        <h1>Talk to Me</h1>
+                        <div className="progress" role="progressbar" aria-label="Basic example">
+                            <div className="progress-bar" style={{ width: `${(7.2 / 10) * 100}%` }}></div>
+                        </div>
+
                     </div>
                     <img src={caraosel} alt="" />
                     <div className="gradient"></div>
                 </div>
             </section>
-        </AuthLayout>
+        </AuthLayout >
     )
 }
 
