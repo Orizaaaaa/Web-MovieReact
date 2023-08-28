cb0017eac9b093a7b44e627cc7939d4661c5e4da
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-08-28 08:03:26 +0700

    finish popular movie

--- src/components/Fragment/PopularMovies.jsx
+++ src/components/Fragment/PopularMovies.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { getMovie } from '../../services/popularMovie'
 import { Splide, SplideSlide } from '@splidejs/react-splide'
 import '@splidejs/react-splide/css';
+import { StarFill } from 'react-bootstrap-icons';
 
 
 
@@ -23,22 +24,34 @@ function PopularMovies() {
                 options={{
                     rewind: true,
                     arrows: false,
-                    type: 'loop',
                     autoplay: true,
                     perPage: 5,
-                    gap: 10,
+                    gap: 5,
                     pagination: false,
                     breakpoints: {
                         992: {
                             perPage: 4,
                         },
+                        730: {
+                            perPage: 3,
+                        }
                     }
 
                 }}
             >
                 {movies.map((movie) => (
                     <SplideSlide key={movie.id}>
-                        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} className="w-100 h-75 mb-3 rounded-3" />
+                        <div className="cardPopular">
+                            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} className="w-100 h-75 mb-3 rounded-3" />
+
+                            <div className="rate mx-auto w-100 p-2">
+                                <h4>{movie.title}</h4>
+                                <div className="progress " role="progressbar" aria-label="Basic example">
+                                    <div className="progress-bar" style={{ width: `${(movie.vote_average / 10) * 100}%` }}></div>
+                                </div>
+                                <p className='ms-1 mt-1 d-flex align-items-center' > Rate {movie.vote_average} <StarFill className='ms-2' /> </p>
+                            </div>
+                        </div>
                     </SplideSlide>
                 ))}
 
